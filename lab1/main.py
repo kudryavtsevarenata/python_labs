da6dcaaf7733138806d659b16200895662283e26
kudryavtsevarenata <kudryavtseva.renata@yandex.ru>
2022-11-01 21:19:59 +0500

    all tasks was done

---
+++ lab1/main.py
@@ -0,0 +1,20 @@
+import random
+from statistics import median
+sum = 0
+mx_num = 888888
+n = mx_num + 1
+for i in range(0, mx_num+1):
+    sum += i
+print("Сумма ряда =", sum)
+print("Среднее этого ряда =", int(sum/n))
+
+a = random.sample(range(0, mx_num), 5)
+sm = 0
+print(a)
+for i in range(0, len(a)):
+    sm += a[i]
+print("Сумма 100 случайных чисел из промежутка =", sm)
+print("Медиана =", median(a))
+
+
+
