ff9d03e98c9d56241ba0a854cb4485734afea5ee
kudryavtsevarenata <kudryavtseva.renata@yandex.ru>
2022-11-02 14:55:28 +0500

    output file added in lab1

--- lab1/main.py
+++ lab1/main.py
@@ -5,15 +5,15 @@ mx_num = 888888
 n = mx_num + 1
 for i in range(0, mx_num+1):
     sum += i
-print("Сумма ряда =", sum)
-print("Среднее этого ряда =", int(sum/n))
+print("Summ =", sum)
+print("Average =", int(sum/n))
 
 a = random.sample(range(0, mx_num), 100)
 sm = 0
 for i in range(0, len(a)):
     sm += a[i]
-print("Сумма 100 случайных чисел из промежутка =", sm)
-print("Медиана =", median(a))
+print("Summ of 100 random integers =", sm)
+print("Median =", median(a))
 
 
 
