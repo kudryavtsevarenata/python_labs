bdab656dbfa6f951491020062f2420b208864abb
kudryavtsevarenata <kudryavtseva.renata@yandex.ru>
2022-11-23 14:25:18 +0500

    new version of lab3 - pyDatalog

--- lab3/main.py
+++ lab3/main.py
@@ -1,12 +1,12 @@
 from pyDatalog import pyDatalog
 import random
-pyDatalog.create_terms('X, Z, y, Sm, P, div, Average, N, Y, SumRand, Med, mas')
-(y['sum'] == sum_(Y, for_each=Y)) <= ((Y.in_(range(1, 888888))))
-print(y['sum'] == Sm)
+pyDatalog.create_terms('X, Z, y, Sm, div, Average, Y, SumRand')
+(y[X] == sum_(Y, for_each=Y)) <= ((Y._in(range_(X+1))))
+print(y[888888] == Sm)
 (div[X, Y] == Z) <= (X // Y == Z)
-print(div[y['sum'], 888888] == Average)
-# y['random_row'] == Z.in_(random.sample(range(0, 20), 7))
-# print(Z.in_(sorted(random.sample(range(0, 20), 7))))
-(y['sum_rand'] == sum_(Z, for_each=Z)) <= Z.in_(random.sample(range(1, 888888), 100))
-print(div[y['sum_rand'], 100] == Med)
+print(div[y[888888], 888888] == Average)
+
+l = [random.choice(range(888888)) for i in range(100)]
+(y['sum_rand'] == sum_(Z, for_each=Z)) <= Z.in_(l)
 print(y['sum_rand'] == SumRand)
+print("Median: ", (l[50] + l[51]) / 2)
